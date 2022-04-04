f69e8648c5a1a3938c87acf1bc5fa6f8e764fc9b
nanian <268353@qq.com>
2022-04-04 14:15:26 +0800

    second commit

--- src/main/java/com/hunakeji/test/test01.java
+++ src/main/java/com/hunakeji/test/test01.java
@@ -3,5 +3,6 @@ package com.hunakeji.test;
 public class test01 {
     public static void main(String[] args) {
         System.out.println("你好我是第一个Git");
+        System.out.println("你好我是第一个Git22");
     }
 }
